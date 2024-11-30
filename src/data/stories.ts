0f219a0e19681210fb3451eacc1135bae082e5f0
alphacoder-mp3 <shindeakshay223@gmail.com>
2024-11-30 20:19:35 +0530

    Add data for stories

---
+++ src/data/stories.ts
@@ -0,0 +1,26 @@
+export const stories = [
+  {
+    id: '1',
+    imageUrl: 'https://images.unsplash.com/photo-1506744038136-46273834b3fb',
+    username: 'nature_lover',
+    timestamp: '2h ago',
+  },
+  {
+    id: '2',
+    imageUrl: 'https://images.unsplash.com/photo-1469474968028-56623f02e42e',
+    username: 'adventure_time',
+    timestamp: '3h ago',
+  },
+  {
+    id: '3',
+    imageUrl: 'https://images.unsplash.com/photo-1447752875215-b2761acb3c5d',
+    username: 'mountain_explorer',
+    timestamp: '4h ago',
+  },
+  {
+    id: '4',
+    imageUrl: 'https://images.unsplash.com/photo-1472214103451-9374bd1c798e',
+    username: 'photo_master',
+    timestamp: '5h ago',
+  },
+];
